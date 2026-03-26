724baed78c038f9eb7c8815d5d88ec969be01f4d
HariBhandari02 <haribhandari0206@gmail.com>
2026-03-26 11:28:50 +0545

    Fetching data success from api

--- codeCamp-GraphQl/index.js
+++ codeCamp-GraphQl/index.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
+import axios from "axios";
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -8,16 +8,18 @@ const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Post {
+   userId: Int
+   id: Int
+   title: String
+   body: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    posts: [Post]
   }
 `;
 const books = [
@@ -34,7 +36,8 @@ const books = [
 // Defining resolver
 const resolvers = {
   Query: {
-    books: () => books,
+    posts: async () =>
+      (await axios.get(`https://jsonplaceholder.typicode.com/posts`)).data,
   },
 };
 
