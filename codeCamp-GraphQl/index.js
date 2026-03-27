59b6e71a29627847ea5a5ad35b13aac217e295a5
HariBhandari02 <haribhandari0206@gmail.com>
2026-03-27 12:54:58 +0545

    typeDefs -> created schema.js file for defining tyes

--- codeCamp-GraphQl/index.js
+++ codeCamp-GraphQl/index.js
@@ -1,27 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import axios from "axios";
+
+// Types
+import { typeDefs } from "codeCamp-GraphQl\schema.js";
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Post {
-   userId: Int
-   id: Int
-   title: String
-   body: String
-  }
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Post {
+//    userId: Int
+//    id: Int
+//    title: String
+//    body: String
+//   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    posts: [Post]
-  }
-`;
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     posts: [Post]
+//   }
+// `;
 const books = [
   {
     title: "The Awakening",
@@ -34,12 +38,12 @@ const books = [
 ];
 
 // Defining resolver
-const resolvers = {
-  Query: {
-    posts: async () =>
-      (await axios.get(`https://jsonplaceholder.typicode.com/posts`)).data,
-  },
-};
+// const resolvers = {
+//   Query: {
+//     posts: async () =>
+//       (await axios.get(`https://jsonplaceholder.typicode.com/posts`)).data,
+//   },
+// };
 
 /* Instances of apollo server
 ApolloServer constructor requires two paramaters :  
---
+++ codeCamp-GraphQl/schema.js
@@ -0,0 +1,45 @@
+export const typeDefs = `#graphql
+
+  type Book {
+    id: ID!
+    title: String!
+    author: String!
+    genre: [String!]!  
+  }
+
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+  }
+
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+
+
+
+
+# Query --> This is reqired 
+# it is to define th entry points 
+  type Query {
+    reviews: [Reviews]
+    games: [Game]
+    authors: [Author]
+
+
+  }
+
+`;
+
+// int, float, string, boolean, ID
+// [String] => for multiple values
+// ! -> Required value
